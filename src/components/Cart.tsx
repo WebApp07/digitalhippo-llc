5c780c4caf6e26c9834dd5afe8a9d16ea408e97b
WebApp07 <aminezouguig@proton.me>
2026-01-14 11:35:28 +0100

    Implement Cart drawer with empty state and checkout link

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -11,9 +11,10 @@ import { Separator } from "./ui/separator";
 import { formatPrice } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 const Cart = () => {
-  const itemCount = 1;
+  const itemCount = 0;
   const fee = 1;
   return (
     <Sheet>
@@ -69,7 +70,31 @@ const Cart = () => {
             </div>
           </>
         ) : (
-          <div className=""></div>
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div
+              aria-hidden="true"
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+            >
+              <Image
+                src="/hippo-empty-cart.png"
+                fill
+                alt="empty shopping cart keyversely"
+              />
+            </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link
+                href="/products"
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-foreground",
+                })}
+              >
+                Browse products and add items to get started
+              </Link>
+            </SheetTrigger>
+          </div>
         )}
       </SheetContent>
     </Sheet>
